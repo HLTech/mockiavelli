b5f92f56f6aca49d7a6e8ec918955dfbd862ad12
lukasz.fiszer <lukasz.fiszer@hltech.com>
2020-05-05 16:38:16 +0200

    chore(semantic-release): fix config

--- release.config.js
+++ release.config.js
@@ -2,7 +2,8 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
         '@semantic-release/npm',
-        '@semantic-release/git',
+        '@semantic-release/github',
     ],
 };
