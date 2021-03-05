bdc9f63dae4e95ca84d466c811d95fae45c11def
Łukasz Fiszer <lukaszfiszer@users.noreply.github.com>
2021-03-05 16:42:35 +0100

    chore(ci): release with github actions (#22)
    * add release job that is triggered on master branch
    * remove semantic-release/git plugin
    * Remove semantic-release/changelog
    * Delete .travis.yml

--- release.config.js
+++ release.config.js
@@ -2,14 +2,7 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        '@semantic-release/changelog',
         '@semantic-release/npm',
-        [
-            '@semantic-release/git',
-            {
-                message: 'chore(release): ${nextRelease.version} [skip ci]',
-            },
-        ],
         '@semantic-release/github',
     ],
 };
