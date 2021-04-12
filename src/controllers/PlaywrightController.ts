8b541ee9cd50669ce506b6f651c83a6d0413806d
Lukasz Fiszer <lukasz.fiszer@gmail.com>
2021-04-12 17:24:02 +0200

    fix(controllers): Mirror puppeteer and playwright types to ensure compat with different versions and packages

--- src/controllers/PlaywrightController.ts
+++ src/controllers/PlaywrightController.ts
@@ -1,4 +1,3 @@
-import playwright from 'playwright-core';
 import {
     BrowserController,
     BrowserRequestHandler,
@@ -10,10 +9,10 @@ import { getOrigin, tryJsonParse } from '../utils';
 import { parse } from 'url';
 
 export class PlaywrightController implements BrowserController {
-    constructor(private readonly page: playwright.Page) {}
+    constructor(private readonly page: PlaywrightPage) {}
 
     async startInterception(onRequest: BrowserRequestHandler) {
-        this.page.route('**/*', (route: playwright.Route) => {
+        await this.page.route('**/*', (route: PlaywrightRoute) => {
             onRequest(
                 this.toBrowserRequest(route.request()),
                 (data) => this.respond(route, data),
@@ -22,7 +21,7 @@ export class PlaywrightController implements BrowserController {
         });
     }
 
-    private toBrowserRequest(request: playwright.Request): BrowserRequest {
+    private toBrowserRequest(request: PlaywrightRequest): BrowserRequest {
         // TODO find a better alternative for url.parse
         const { pathname, query, protocol, host } = parse(request.url(), true);
 
@@ -39,7 +38,7 @@ export class PlaywrightController implements BrowserController {
         };
     }
 
-    private async respond(route: playwright.Route, response: ResponseData) {
+    private async respond(route: PlaywrightRoute, response: ResponseData) {
         await route.fulfill({
             headers: response.headers || {},
             status: response.status,
@@ -48,14 +47,101 @@ export class PlaywrightController implements BrowserController {
         });
     }
 
-    private async skip(route: playwright.Route) {
+    private async skip(route: PlaywrightRoute) {
         await route.continue();
     }
 
     /**
      * Obtain request origin url from originating frame url
      */
-    private getRequestOrigin(request: playwright.Request): string {
+    private getRequestOrigin(request: PlaywrightRequest): string {
         return getOrigin(request.frame().url());
     }
+}
+
+/**
+ * Mirror of playwright's Page interface
+ */
+export interface PlaywrightPage {
+    route(
+        url: string,
+        handler: (route: PlaywrightRoute, request: PlaywrightRequest) => void
+    ): Promise<void>;
+}
+
+/**
+ * Mirror of playwright's Route interface
+ */
+interface PlaywrightRoute {
+    fulfill(response: PlaywrightRouteFulfillResponse): Promise<void>;
+    request(): PlaywrightRequest;
+    continue(): Promise<void>;
+}
+
+/**
+ * Mirror of playwright's Response interface
+ */
+interface PlaywrightRouteFulfillResponse {
+    /**
+     * Response status code, defaults to `200`.
+     */
+    status?: number;
+
+    /**
+     * Optional response headers. Header values will be converted to a string.
+     */
+    headers?: { [key: string]: string };
+
+    /**
+     * If set, equals to setting `Content-Type` response header.
+     */
+    contentType?: string;
+
+    /**
+     * Optional response body.
+     */
+    body?: string | Buffer;
+
+    /**
+     * Optional file path to respond with. The content type will be inferred from file extension. If `path` is a relative path, then it is resolved relative to current working directory.
+     */
+    path?: string;
+}
+
+/**
+ * Mirror of playwright's Request interface
+ */
+export interface PlaywrightRequest {
+    /**
+     * @returns A Frame that initiated this request.
+     */
+    frame(): {
+        url(): string;
+    };
+
+    /**
+     * @returns An object with HTTP headers associated with the request. All header names are lower-case.
+     */
+    headers(): { [key: string]: string };
+
+    /**
+     * @returns Request's method (GET, POST, etc.)
+     */
+    method(): string;
+
+    /**
+     * @returns Request's post body, if any.
+     */
+    postData(): null | string;
+
+    /**
+     * Contains the request's resource type as it was perceived by the rendering engine.
+     * ResourceType will be one of the following: `document`, `stylesheet`, `image`, `media`, `font`, `script`, `texttrack`, `xhr`, `fetch`, `eventsource`, `websocket`, `manifest`, `other`.
+     */
+    resourceType(): string;
+
+    /**
+     * @returns URL of the request.
+     */
+    url(): string;
 }
--- src/controllers/PuppeteerController.ts
+++ src/controllers/PuppeteerController.ts
@@ -1,4 +1,3 @@
-import { Page, Request } from 'puppeteer';
 import { parse } from 'url';
 import {
     BrowserController,
@@ -8,7 +7,7 @@ import {
 import { getOrigin, tryJsonParse } from '../utils';
 
 export class PuppeteerController implements BrowserController {
-    constructor(private readonly page: Page) {}
+    constructor(private readonly page: PuppeteerPage) {}
 
     async startInterception(onRequest: BrowserRequestHandler) {
         await this.page.setRequestInterception(true);
@@ -21,7 +20,7 @@ export class PuppeteerController implements BrowserController {
         });
     }
 
-    private toBrowserRequest(request: Request): BrowserRequest {
+    private toBrowserRequest(request: PuppeteerRequest): BrowserRequest {
         // TODO find a better alternative for url.parse
         const { pathname, query, protocol, host } = parse(request.url(), true);
 
@@ -41,7 +40,94 @@ export class PuppeteerController implements BrowserController {
     /**
      * Obtain request origin url from originating frame url
      */
-    private getRequestOrigin(request: Request) {
+    private getRequestOrigin(request: PuppeteerRequest) {
         return getOrigin(request.frame()?.url());
     }
+}
+
+/**
+ * Mirror of puppeteer Page interface
+ */
+export interface PuppeteerPage {
+    setRequestInterception(enabled: boolean): Promise<void>;
+    on(eventName: 'request', handler: (e: PuppeteerRequest) => void): any;
+}
+
+/**
+ * Mirror of puppeteer Request interface
+ */
+interface PuppeteerRequest {
+    /**
+     * Continues request with optional request overrides.
+     * To use this, request interception should be enabled with `page.setRequestInterception`.
+     * @throws An exception is immediately thrown if the request interception is not enabled.
+     */
+    continue(): Promise<void>;
+
+    /**
+     * @returns The `Frame` object that initiated the request, or `null` if navigating to error pages
+     */
+    frame(): {
+        url(): string;
+    } | null;
+
+    /**
+     * An object with HTTP headers associated with the request.
+     * All header names are lower-case.
+     */
+    headers(): Record<string, string>;
+
+    /** Returns the request's method (GET, POST, etc.) */
+
+    method(): 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE' | 'OPTIONS';
+
+    /** Contains the request's post body, if any. */
+    postData(): string | undefined;
+
+    /** Contains the request's resource type as it was perceived by the rendering engine.  */
+    resourceType():
+        | 'document'
+        | 'stylesheet'
+        | 'image'
+        | 'media'
+        | 'font'
+        | 'script'
+        | 'texttrack'
+        | 'xhr'
+        | 'fetch'
+        | 'eventsource'
+        | 'websocket'
+        | 'manifest'
+        | 'other';
+
+    /**
+     * Fulfills request with given response.
+     * To use this, request interception should be enabled with `page.setRequestInterception`.
+     * @throws An exception is immediately thrown if the request interception is not enabled.
+     * @param response The response options that will fulfill this request.
+     */
+    respond(response: PuppeteerRespondOptions): Promise<void>;
+
+    /** A matching `Response` object, or `null` if the response has not been received yet. */
+    response(): Response | null;
+
+    /** Contains the URL of the request. */
+    url(): string;
+}
+
+/**
+ * Mirror of puppeteer Response interface
+ */
+interface PuppeteerRespondOptions {
+    /**
+     * Specifies the response status code.
+     * @default 200
+     */
+    status?: number;
+    /** Specifies the response headers. */
+    headers?: Record<string, string>;
+    /** Specifies the Content-Type response header. */
+    contentType?: string;
+    /** Specifies the response body. */
+    body?: Buffer | string;
 }
